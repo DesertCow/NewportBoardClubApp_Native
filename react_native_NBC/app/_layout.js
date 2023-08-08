8fd4fcfd33f73a378c27e8ec8fc4a404d76d8bf8
Clayton S <desertcow07@gmail.com>
2023-08-08 12:00:06 -0700

    First app not working... EOS

---
+++ react_native_NBC/app/_layout.js
@@ -0,0 +1,13 @@
+// import { Stack } from 'expo-router';
+
+// const Layout = () => {
+//   return <Stack />;
+// }
+
+// export default Layout;
+
+import { Stack } from 'expo-router/stack';
+
+export default function Layout() {
+  return <Stack />;
+}
