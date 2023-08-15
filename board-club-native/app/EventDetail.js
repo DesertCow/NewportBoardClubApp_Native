0b3f7c02beef482d031761b8ecd65c54837c03d0
Clayton S <desertcow07@gmail.com>
2023-08-15 15:02:09 -0700

    Got basic event detail page working, WIP / EOS

--- board-club-native/app/EventDetail.js
+++ board-club-native/app/EventDetail.js
@@ -1,4 +1,8 @@
-import { Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, ScrollView, Image } from 'react-native';
+
+//* GraphQL
+import { getEvent_Q } from '../utils/queries';
+import { useQuery } from '@apollo/client';
 
 //* Components Import
 import Header from "../components/common/Header";
@@ -6,28 +10,71 @@ import Footer from "../components/common/Footer";
 
 function EventDetail(nav) {
 
-  const EventID = nav.route.params.EventID;
+  const eventID = nav.route.params.EventID;
+
+      //* Get requested Event from Database
+    var { loading, data } = useQuery(getEvent_Q, {
+      variables: { eventId: eventID },
+    });
 
   // console.log("EventID = " + EventID)
-  console.log(nav.route.params.EventID)
+  console.log(nav.route.params.eventID)
 
-  return (
+  if(!loading){
 
-    <SafeAreaView style={{ flex: 1}}>
-      
-      <Header  style={{ position: "absolute"}}></Header>
+    console.log(data.getEvent)
 
-      <ScrollView>
-        <Text>Event Detail Page ({EventID})</Text>
-        {/* <Text>Event Detail Page</Text> */}
-      </ScrollView>
-      
-      <Footer></Footer>
+    const eventData = data.getEvent
 
-    </SafeAreaView>
+    return (
 
-  );
+      <SafeAreaView style={{ flex: 1}}>
+        
+        <Header  style={{ position: "absolute"}}></Header>
 
+        <ScrollView>
+          {/* <Text>Event Detail Page ({eventID})</Text> */}
+
+          <Text style={styles.eventTitleText}>{eventData.eventName}</Text>
+
+          <Image
+            style={styles.eventPicture}
+            source={{uri: eventData.eventPhotoURL}}
+            />
+
+          <Text style={styles.eventDateText}>{eventData.eventDate}</Text>
+
+        </ScrollView>
+        
+        <Footer></Footer>
+
+      </SafeAreaView>
+
+    );
+    }
 }
+
+const styles = StyleSheet.create({
+  eventPicture: {
+    marginVertical: 30,
+    alignSelf: 'center',
+    height: 250,
+    width: 300,
+    borderRadius: 30,
+  },
+  eventTitleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  eventDateText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+
+})
 
 export default EventDetail;
