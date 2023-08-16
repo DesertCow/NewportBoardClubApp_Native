b89b044f6ab99c19b72b0ec404354b59b235a8c0
Clayton S <desertcow07@gmail.com>
2023-08-15 17:48:44 -0700

    Enabled live weather data in header

--- board-club-native/components/common/Header.js
+++ board-club-native/components/common/Header.js
@@ -1,5 +1,10 @@
 import { StyleSheet, View, Image, Text, Platform } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+
+//* GraphQL
+import { useQuery } from '@apollo/client';
+import { getWX_Q } from '../../utils/queries';
 
 //* Icon Import
 import { FontAwesome, FontAwesome5, MaterialCommunityIcons, Feather, Ionicons   } from '@expo/vector-icons';
@@ -9,59 +14,138 @@ const boardClubIcon = require('../../assets/img/BC_Logo_Clear_1.png');
 
 
 //? Temp Status set till API call working
-const ClubOpen = false;
+// let ClubOpen = false;
 
 
 
 function Header() {
 
   const navigation = useNavigation();
+  // const [ClubOpen, setClubOpen] = React.useState(true);
 
-  let clubOpenText = "Closed";
+  //* Get Latest Weather Data from App Server
+  var { loading, data } = useQuery(getWX_Q)
 
-  if(ClubOpen) {
-    clubOpenText = "Open";
+  if(!loading){
+
+      let currentClubStatus;
+
+      if(data.getWX.clubStatus == true)
+      {
+        currentClubStatus = "Open"
+        // setClubOpen(true)
+        // currentClubStatus = "Closed"
+      }else{
+        currentClubStatus = "Closed"
+        // setClubOpen(false)
+      }
+
+    if(currentClubStatus == "Open"){
+
+      console.log(data)
+
+      //* Logic for Tide Direction Icon
+      if (data.getWX.tideRise) {
+          // tideDirIcon = <img src={require("../../img/Tide_Rising.png")} className="headerTideDir" alt="Tide Icon" />
+          console.log("Tide Rising")
+      } 
+
+      if (!data.getWX.tideRise) {
+          // tideDirIcon = <img src={require("../../img/Tide_Falling.png")} className="headerTideDir" alt="Tide Icon" />
+          console.log("Tide Falling")
+      }
+
+      return(
+
+        <View style={styles.header}>
+
+          <View style={styles.logoCol}>
+            <Image
+              style={styles.NBCLogo}
+              source={boardClubIcon}
+            />
+            <Text style={styles.clubHouseStatusText}> Club House: <Text style={{color: "green"}}>{currentClubStatus}</Text></Text>
+          </View>
+
+          <View style={styles.wxBoxCol}>
+            <View style={styles.wxBoxRow}>
+              <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
+              <Feather name="wind" size={30} color="black" />
+              <Text style={styles.wxDataText}> {data.getWX.wind} mph</Text>
+            </View>
+
+            <View style={styles.wxBoxRow}>
+              <Text style={styles.wxDataText}> {data.getWX.tideMSL} ft</Text>
+              <Ionicons name="sunny" size={24} color="black" />
+              <Text style={styles.wxDataText}> {data.getWX.airTemp} &deg;F</Text>
+            </View>
+
+            <View style={styles.wxBoxRow}>
+              <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
+              <MaterialCommunityIcons name="coolant-temperature" size={30} color="black" />
+              <Text style={styles.wxDataText}> {data.getWX.waterTemp} &deg;F</Text>
+            </View>
+            
+          </View>
+
+        </View>
+
+      )
   }
-  else {
-    clubOpenText = "Closed";
-  }
 
-  return(
+  if(currentClubStatus == "Closed"){
 
-    <View style={styles.header}>
+    console.log(data)
 
-      <View style={styles.logoCol}>
-        <Image
-          style={styles.NBCLogo}
-          source={boardClubIcon}
-        />
-        <Text style={styles.clubHouseStatusText}> Club House: <Text style={styles.clubHouseColoredText}>{clubOpenText}</Text></Text>
+    //* Logic for Tide Direction Icon
+    if (data.getWX.tideRise) {
+        // tideDirIcon = <img src={require("../../img/Tide_Rising.png")} className="headerTideDir" alt="Tide Icon" />
+        console.log("Tide Rising")
+    } 
+
+    if (!data.getWX.tideRise) {
+        // tideDirIcon = <img src={require("../../img/Tide_Falling.png")} className="headerTideDir" alt="Tide Icon" />
+        console.log("Tide Falling")
+    }
+
+    return(
+
+      <View style={styles.header}>
+
+        <View style={styles.logoCol}>
+          <Image
+            style={styles.NBCLogo}
+            source={boardClubIcon}
+          />
+          <Text style={styles.clubHouseStatusText}> Club House: <Text style={{color: "red"}}>{currentClubStatus}</Text></Text>
+        </View>
+
+        <View style={styles.wxBoxCol}>
+          <View style={styles.wxBoxRow}>
+            <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
+            <Feather name="wind" size={30} color="black" />
+            <Text style={styles.wxDataText}> {data.getWX.wind} mph</Text>
+          </View>
+
+          <View style={styles.wxBoxRow}>
+            <Text style={styles.wxDataText}> {data.getWX.tideMSL} ft</Text>
+            <Ionicons name="sunny" size={24} color="black" />
+            <Text style={styles.wxDataText}> {data.getWX.airTemp} &deg;F</Text>
+          </View>
+
+          <View style={styles.wxBoxRow}>
+            <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
+            <MaterialCommunityIcons name="coolant-temperature" size={30} color="black" />
+            <Text style={styles.wxDataText}> {data.getWX.waterTemp} &deg;F</Text>
+          </View>
+          
+        </View>
+
       </View>
 
-      <View style={styles.wxBoxCol}>
-        <View style={styles.wxBoxRow}>
-          <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
-          <Feather name="wind" size={30} color="black" />
-          <Text style={styles.wxDataText}> 4mph</Text>
-        </View>
-
-         <View style={styles.wxBoxRow}>
-          <Text style={styles.wxDataText}> 3.9 ft</Text>
-          <Ionicons name="sunny" size={24} color="black" />
-          <Text style={styles.wxDataText}> 73 &deg;F</Text>
-        </View>
-
-        <View style={styles.wxBoxRow}>
-          <MaterialCommunityIcons name="waves-arrow-up" size={30} color="black" />
-          <MaterialCommunityIcons name="coolant-temperature" size={30} color="black" />
-          <Text style={styles.wxDataText}> 65 &deg;F</Text>
-        </View>
-        
-      </View>
-
-    </View>
-
-  )
+    )
+    }
+  }
 };
 
 
@@ -80,7 +164,7 @@ const styles = StyleSheet.create({
     marginTop: 5,
   },
   clubHouseColoredText: {
-    color: ClubOpen === true ? "green" : "red",
+    // color: ClubOpen === true ? "green" : "red",
   },
   logoCol: {
     alignSelf: "flex-start",
