0f7fd63a4f65f578889f3c7e9d8a913130ce87d6
Clayton S <desertcow07@gmail.com>
2023-08-28 21:41:38 -0700

    Got surf sessions to load and build with hardcoded ID, still working on Auth to return user ID without causing error

--- board-club-native/app/ListOfSessions.js
+++ board-club-native/app/ListOfSessions.js
@@ -1,4 +1,4 @@
-import { Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, ScrollView, ActivityIndicator, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 
@@ -21,19 +21,21 @@ function ListOfSession() {
 
   const navigation = useNavigation();
   const [profileData, setProfileData] = React.useState(null);
-  const [surfSessionData, setSurfSessionData] = React.useState(null);
+  // const [surfSessionData2, setSurfSessionData2] = React.useState([]);
 
-  var finalSurfHackListView = []
+  var surfSessionListView = []
+  var tempID = "64e27081d2fa09f5f6881882"
+  // var tempID = "64e27081d2fa09f5f6881865"
 
-  console.log("!!!!!!!!!!!!!!!!!!!!!!!")
-  console.log(finalSurfHackListView)
+  // console.log("!!!!!!!!!!!!!!!!!!!!!!!")
+  // console.log(finalSurfHackListView)
 
   
   
-  const displayHackDetails = async (event, reqSurfHackID) => {
+  const displaySurfSessionDetails = async (event, reqSurfSessionID) => {
   
     navigation.navigate('SurfHackDetail', {
-      HackID: reqSurfHackID
+      SurfSessionID: reqSurfSessionID
     });
 
   };
@@ -44,55 +46,77 @@ function ListOfSession() {
     let profile = await Auth.getProfile()
     // profile = profile.data
 
-    setProfileData(profile.data)
+    // setProfileData(profile.data)
 
-    console.log("Profile = ")
-    console.log(profile)
+    // console.log("Profile = ")
+    // console.log(profile)
 
-    // console.log(profile._id)
+    // if(!loading) {
 
-    // //* Get List of surf sessions for user from Database
-    // var { loading, data } = useQuery(getSurfSessionList_Q, {
-    //   // variables: { userId: loadProfile()._id },
-    //   variables: { userId: profile._id },
-    // });
-
-    
-    
-
-    if(!loading) {
-
-      console.log("111LOADING = ")
-      console.log(loading)
-      console.log("Data = ")
-      console.log(data)
-      setSurfSessionData(data)
+    //   console.log("111LOADING = ")
+    //   console.log(loading)
+    //   console.log("Data = ")
+    //   console.log(data)
+    //   setSurfSessionData(data)
 
 
-      return loading
-    }
+    //   return data
+    // }
 
     // return loading
+    return profile
 
   }
 
+  //* Build Buttons Based on Surf Session data from Database
+  async function buildSurfSessionView(sessionData) {
+
+    console.log("*****************************************")
+    console.log(sessionData)
+    
+    //* Generate IMG and Button for each event
+      surfSessionListView.push(
+
+      <View key={sessionData._id}>
+        
+        {/* <Text style={styles.sessionTitleText}>{sessionData.hackTitle}</Text> */}
+
+        {/* <Image
+        style={styles.hackPicture}
+        source={{uri: surfHackData.hackPhotoURL}}
+        /> */}
+
+        <TouchableOpacity
+          style={styles.sessionButton}
+          // backgroundColor={switchColor}
+          onPress={(event) => displaySurfSessionDetails(event, sessionData._id)}>
+          <Text style={styles.buttonText}>{sessionData.sessionDate} ({sessionData.sessionTime}) @ {sessionData.sessionLocation}</Text>
+        </TouchableOpacity>
+
+      </View>
+    )
+  }
+
+  //* Request Surf Session data from Database
   async function loadSurfSessionData() {
 
+    let profile = null
     // profile = await loadProfile()
 
-    // console.log("PROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
+    console.log("PROOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO")
     // console.log(profile)
 
-    // //* Get List of surf sessions for user from Database
-    // var { loading, data } = useQuery(getSurfSessionList_Q, {
-    //   variables: { userId: profile._id },
-    // });
+    // if(profile != null) {
+      // //* Get List of surf sessions for user from Database
+      var { loading, data } = useQuery(getSurfSessionList_Q, {
+        // variables: { userId: profile._id },
+        variables: { userId: tempID },
+      });
 
-    console.log("Data@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@r")
-    console.log(data)
-    console.log(loading)
+      return data
+    // }
 
-    return true
+    // return undefined
 
   }
 
@@ -105,50 +129,95 @@ function ListOfSession() {
   // let loadStatus = loadSurfSessionData()
 
   //* Load Profile Data/JWT Token
-  loadProfile()
+  // loadProfile()
 
-  // console.log(profileData)
-  // console.log("LOADING = ")
-  // console.log(loading)
+  //* Load Surf Session Data from DB
+  let surfSessionData = loadSurfSessionData()
+  surfSessionData = surfSessionData._j
 
-  if(profileData !== null)
+  //* Check surfSessionData has loaded
+  if(surfSessionData !== undefined)
   {
-    console.log("Profile Data Valid!")
-    console.log(profileData)
+    surfSessionData = surfSessionData.getAllUsersSurfSession
 
-    // profile = profileData
+    //* Check to confirm session Data exists
+    if(surfSessionData.length > 0)
+    // if(false)
+    {
+      // console.log("Profile Data Valid!")
+      // console.log(profileData)
+
+      // surfSessionData = surfSessionData.getAllUsersSurfSession
+
+      console.log("%%%%%%%%%%%%%%%%%%%%%")
+      console.log(surfSessionData)
+      console.log(surfSessionData[0]._id)
+      console.log(surfSessionData[1]._id)
+      console.log(surfSessionData[2]._id)
+
+      // profile = profileData
 
 
 
-    // let loading = true
+      // let loading = true
 
-    //* Get List of surf sessions for user from Database
-    // var { loading, data } = useQuery(getSurfSessionList_Q, {
-    //   // variables: { userId: loadProfile()._id },
-    //   variables: { userId: profileData._id },
-    // });
+      //* Get List of surf sessions for user from Database
+      // var { loading, data } = useQuery(getSurfSessionList_Q, {
+      //   // variables: { userId: loadProfile()._id },
+      //   variables: { userId: profileData._id },
+      // });
 
-    // if(!loading) {
+      // if(!loading) {
 
-      // console.log(loading)
-      // console.log(data)
+        // console.log(loading)
+        // console.log(data)
 
-    return (
+      // populateListOfSessions(surfSessionData)
+      surfSessionData.forEach(buildSurfSessionView)
 
-      <SafeAreaView style={{ flex: 1}}>
-        
-        <Header  style={{ position: "absolute"}}></Header>
+      return (
 
-        <ScrollView>
-          <Text>List Of Surf Session!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</Text>
-        </ScrollView>
-        
-        <Footer></Footer>
+        <SafeAreaView style={{ flex: 1}}>
+          
+          <Header  style={{ position: "absolute"}}></Header>
 
-      </SafeAreaView>
+          <ScrollView>
 
-    );
+            <Text style={styles.surfSessionTitle}>Surf Sessions</Text>
 
+            <View>
+              {surfSessionListView}
+            </View>
+
+          </ScrollView>
+          
+          <Footer></Footer>
+
+        </SafeAreaView>
+
+      );
+
+    }
+    else {
+
+      return (
+
+        <SafeAreaView style={{ flex: 1}}>
+          
+          <Header  style={{ position: "absolute"}}></Header>
+
+          <ScrollView>
+            <Text>No Surf Sessions Logged!</Text>
+            {/* <ActivityIndicator style={{ marginTop: 250}} size="large" /> */}
+          </ScrollView>
+          
+          <Footer></Footer>
+
+        </SafeAreaView>
+
+      );
+
+    }
   }
   else {
 
@@ -159,7 +228,8 @@ function ListOfSession() {
         <Header  style={{ position: "absolute"}}></Header>
 
         <ScrollView>
-          <Text>Loading!!!!!!!!!!!!</Text>
+          {/* <Text>Loading!!!!!!!!!!!!</Text> */}
+          <ActivityIndicator style={{ marginTop: 250}} size="large" />
         </ScrollView>
         
         <Footer></Footer>
@@ -171,4 +241,49 @@ function ListOfSession() {
   }
 }
 
+const styles = StyleSheet.create({
+  surfSessionTitle: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: "center",
+    marginVertical: 10
+  },
+  sessionDisplayBox: {
+    borderStyle: "solid",
+    borderWidth: 10,
+    borderColor: "#6f6f6f75",
+    justifyContent: "center"
+    
+  },
+  sessionPicture: {
+    marginVertical: 30,
+    alignSelf: 'center',
+    height: 250,
+    width: 300,
+    borderRadius: 30,
+  },
+  sessionTitleText: {
+    fontSize: 23,
+    fontWeight: "bold",
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  sessionButton: {
+    alignItems: 'center',
+    // backgroundColor: "#51A7FF",
+    backgroundColor: "#6f6f6f75",
+    color: "#FFFFFF",
+    height: 50,
+    width: "85%",
+    marginLeft: "8%",
+    marginVertical: 10,
+    justifyContent: 'center',
+    borderRadius: 20,
+    
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  }
+})
 export default ListOfSession;
